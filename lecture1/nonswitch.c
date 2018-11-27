2d99441964d7a0014f37f5b21a096fa65360ea6c
kyawthet8796 <42841091+kyawthet8796@users.noreply.github.com>
2018-11-27 16:56:52 +0630

    Add files via upload

---
+++ lecture1/nonswitch.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    char c = get_char("Answer: ");
+
+    if (c == 'Y' || c == 'y')
+    {
+        printf("yes\n");
+    }
+    else if (c == 'N' || c == 'n')
+    {
+        printf("no\n");
+    }
+    else
+    {
+        printf("error\n");
+    }
+}
