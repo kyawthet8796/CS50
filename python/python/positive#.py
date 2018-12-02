f5713fa8f73dceba6c1c18681778a761286243b9
kyawthet8796 <42841091+kyawthet8796@users.noreply.github.com>
2018-12-03 01:47:53 +0630

    Add files via upload

---
+++ python/python/positive#.py
@@ -0,0 +1,11 @@
+from cs50 import get_int
+
+# Prompt user for a positive number
+while True:
+    n = get_int("Positive number: ")
+    if n > 0:
+        break
+
+# Print out that many bricks
+for i in range(n):
+    print("#")
---
+++ python/python/struct1.py
@@ -0,0 +1,22 @@
+import csv
+from cs50 import get_string
+#from student import Student
+
+# Space for students
+students = []
+
+class Student:
+    def __init__(self, name, dorm):
+        self.name = name
+        self.dorm = dorm
+
+# Prompt for students' names and dorms
+for i in range(3):
+    name = get_string("name: ")
+    dorm = get_string("dorm: ")
+    students.append(Student(name, dorm))
+
+with open("students.csv", "w") as file:
+    writer = csv.writer(file)
+    for student in students:
+        writer.writerow((student.name, student.dorm))
