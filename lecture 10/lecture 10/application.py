4cc0da2a71d5f150a76b2c5c7d0d7a48cbe563de
kyawthet8796 <42841091+kyawthet8796@users.noreply.github.com>
2018-12-03 20:41:58 +0630

    Add files via upload

--- lecture 10/lecture 10/application.py
+++ lecture 10/lecture 10/application.py
@@ -1,27 +1,12 @@
-from flask import Flask, redirect, render_template, request, session
-from flask_session import Session
+from flask import Flask, render_template, request
 
-# Configure app
+from cs50 import eprint, SQL
+
 app = Flask(__name__)
 
-# Configure sessions
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
+db = SQL("sqlite:///lecture.db")
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-
-
-@app.route("/update", methods=["POST"])
-def update():
-    for item in request.form:
-        session[item] = int(request.form.get(item))
-    return redirect("/cart")
-
-
-@app.route("/cart")
-def cart():
-    return render_template("cart.html", cart=session)
+    rows = db.execute("SELECT * FROM Album")
+    return render_template("index.html", albums=rows)
